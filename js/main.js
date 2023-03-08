fa0a874205fd0cda8325f0161a8addfd01295063
Luis <luismiguelotero2000@gmail.com>
2023-03-08 13:27:27 -0500

    PreEntrega2

--- js/main.js
+++ js/main.js
@@ -1,22 +1,67 @@
+let deudores = [];
+const button = document.getElementById("btnAdd");
+const button1 = document.getElementById("btnShow")
+class Deudor {
+    constructor(nombre, deuda) {
+        this.nombre = nombre
+        this.deuda = deuda
+    }
+    mensaje() {
+        alert("El usuario: " + this.nombre + " " + "Su deuda es: " + this.deuda)
+    }
+}
+
+button.addEventListener('click', () => {
+    addUser()
+});
 function calculateInterest(principal, rate, months) {
     return principal * rate * months / 100;
 }
+function addUser() {
+    let nombreUsuario = prompt("Ingrese Usuario")
+    console.log(nombreUsuario);
+    if (!(nombreUsuario == null || nombreUsuario == "")) {
+        const principal = prompt("CANTIDAD DEL PRESTAMO"); // Monto del préstamo
+        const rate = 2.5 / 12; // Tasa mensual convertida a tasa mensual
+        const months = 12; // Número de meses
+        const interest = calculateInterest(principal, rate, months);
+        alert("El interés es de $" + interest);
 
-const principal = prompt("CANTIDAD DEL PRESTAMO"); // Monto del préstamo
-const rate = 2.5 / 12; // Tasa mensual convertida a tasa mensual
-const months = 12; // Número de meses
-const interest = calculateInterest(principal, rate, months);
-
-alert("El interés es de $" + interest);
-
-
-let nombreUusuario = prompt("Ingrese Usuario")
-alert("El usuario ingresado es: " + nombreUusuario + " " + "Su deuda es de " + interest)
-
-class deudor{
-    constructor(nombre, deuda){
-    this.nombre = nombreUusuario
-    this.deuda = interest
+        const deudor = new Deudor(nombreUsuario, interest)
+        deudores.push(deudor);
+        console.log(nombreUsuario, interest);
+        deudor.mensaje()
     }
 }
-alert("El usuario: " + this.nombre + " " + "Tiene una deuda de: " + this.deuda);
+
+button1.addEventListener('click', () => {
+    showUser()
+});
+
+function showUser() {
+    for (const deudor1 of deudores) {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h3> Deudor: ${deudor1.nombre}</h3>
+                                <h3> Deuda: ${deudor1.deuda}</h3>`;
+        document.body.appendChild(contenedor);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
